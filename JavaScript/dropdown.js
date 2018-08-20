9892cd29a7db81eab4eeae2f2e7b63623d203035
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-08-20 13:02:23 +0300

    Added 2 Drop down menus.

---
+++ JavaScript/dropdown.js
@@ -0,0 +1,22 @@
+function mySecondFunction() {
+    document.getElementById("myDropdown2").classList.toggle("show");
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.btn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
