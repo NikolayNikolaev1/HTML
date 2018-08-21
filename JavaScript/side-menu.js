fb2619726295a33f432b106c8572b1c931959534
NikolayNikolaev1 <n.nikolaevBG97@gmail.com>
2018-08-21 17:53:22 +0300

    Added checkbox, side menu to responsive. Not finished.

---
+++ JavaScript/side-menu.js
@@ -0,0 +1,55 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function myNavFunc() {
+    var x = document.getElementById("demoNav");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className =
+            x.previousElementSibling.className.replace(" w3-green", "");
+    }
+}
+
+function myLoginFunc() {
+    var x = document.getElementById("demoLogin");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className =
+            x.previousElementSibling.className.replace(" w3-green", "");
+    }
+}
+
+function myLangFunc() {
+    var x = document.getElementById("demoLang");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className =
+            x.previousElementSibling.className.replace(" w3-green", "");
+    }
+}
+
+function myFilterFunc() {
+    var x = document.getElementById("demoFilter");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className =
+            x.previousElementSibling.className.replace(" w3-green", "");
+    }
+}
